f72916903991e1350170d7506db84b7ca10e82f0
nandhancse <nandhan2884@gmail.com>
2025-12-13 20:13:32 +0530

    new change

--- DjangoProjectFood/settings.py
+++ DjangoProjectFood/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pqn_pi4_yy^a3nk^d_%c)@q_2ao#rnsf@!6vzcep5stk28ssmg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app","127.0.0.1","localhost"]
 
